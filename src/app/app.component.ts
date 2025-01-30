b93828972c81c72566fb2719f20ac03254e2d8f0
Andrew Gross <andrew144@sbcglobal.net>
2025-01-30 04:26:10 -0500

    more updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {WA_LOCAL_STORAGE, WA_WINDOW} from '@ng-web-apis/common';
 import {TUI_DARK_MODE, TUI_DARK_MODE_KEY, TuiButton} from '@taiga-ui/core';
+import {TuiHeader} from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot, TuiButton],
+  imports: [RouterOutlet, TuiRoot, TuiButton, TuiHeader],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
