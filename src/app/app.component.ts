140abc447a7455d08ad21da4a2d7de29e3e376a0
Andrew Gross <andrew144@sbcglobal.net>
2025-01-30 18:00:12 -0500

    some cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,18 +13,7 @@ import {TuiHeader} from '@taiga-ui/layout';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'PortfolioWebsite'; 
-
-  private readonly key = inject(TUI_DARK_MODE_KEY);
-  private readonly storage = inject(WA_LOCAL_STORAGE);
-  private readonly media = inject(WA_WINDOW).matchMedia('(prefers-color-scheme: dark)');
-
   protected readonly darkMode = inject(TUI_DARK_MODE);
-
-  protected reset(): void {
-      this.darkMode.set(this.media.matches);
-      this.storage.removeItem(this.key);
-  }
 
   get themeIcon(): string {
     return this.darkMode() ? '@tui.moon' : '@tui.sun';
