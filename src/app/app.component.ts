1167c7462d5169505634727744e8559e4689c9b3
Andrew Gross <andrew144@sbcglobal.net>
2025-02-03 03:43:57 -0500

    some more updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,12 @@ import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {TUI_DARK_MODE, TuiAppearance, TuiButton, TuiTitle, TuiIcon} from '@taiga-ui/core';
 import {TuiHeader, TuiCardLarge} from '@taiga-ui/layout';
 import { ProjectsComponent } from "./projects/projects.component";
-import { AboutComponent } from "./about/about.component";
 import { ExperienceComponent } from "./experience/experience.component";
 import { P5SketchComponent } from "./p5-sketch/p5-sketch.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot, TuiButton, TuiHeader, TuiAppearance, TuiTitle, TuiIcon, ProjectsComponent, AboutComponent, ExperienceComponent, P5SketchComponent, TuiCardLarge],
+  imports: [RouterOutlet, TuiRoot, TuiButton, TuiHeader, TuiAppearance, TuiTitle, TuiIcon, ProjectsComponent, ExperienceComponent, P5SketchComponent, TuiCardLarge],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -18,11 +18,11 @@ type Card = {
 export class ProjectsComponent {
   protected readonly darkMode = inject(TUI_DARK_MODE);
   cards: Card[] = [
-    {title: 'Trend Wars', content: 'A spunky multiplayer word game made using the Google Trends API, where you attempt to pair a word with the prompt in order to create the most trending phrase.', link: 'www.trendwars.net'},
+    {title: 'Trend Wars', content: 'A spunky multiplayer word game made using the Google Trends API, where you attempt to pair a word with the prompt in order to create the most trending phrase.', link: 'http://trendwars.net'},
     {title: 'Data Stream', content: 'A movement-focused platformer where you play as a virus in the bloodstream of a cyborg.', link: 'https://cwru-ecse390.itch.io/data-stream-f2022-team-3'},
     {title: 'The Rite of Redemption', content: 'A top-down puzzle/adventure game inspired by Legend of Zelda.', link: 'https://ecse-csds290.itch.io/rise-s2021-team-5'},
     {title: 'Markov Lyrics', content: 'A song lyrics text generator that uses Markov chains to combine the styles of any musician or body of text you input.', link: 'https://github.com/andrew0144/markov_lyrics'},
     {title: '8 Puzzle Solver', content: 'Solves any instance of an 8-puzzle using A* search and local beam search, printing output and statistics about the algorithms along the way. ', link: 'https://github.com/andrew0144/CSDS393_P1'},
-    {title: 'This Site', content: 'My new and improved portfolio website. Made using Angular/TypeScript, Taiga UI, p5.js, HTML and less (CSS Preprocessor).', link: ''},
+    {title: 'This Site', content: 'My new and improved portfolio website. Made using Angular/TypeScript, Taiga UI, p5.js, HTML and less (CSS Preprocessor).', link: 'https://github.com/andrew0144/PortfolioWebsite'},
   ];
 }
