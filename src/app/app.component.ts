bb971d9ed53205b3ec2991381d76e8d4a655145d
Andrew Gross <andrew144@sbcglobal.net>
2025-02-03 03:02:39 -0500

    Add some content to the experience section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { TuiRoot } from "@taiga-ui/core";
 import { RouterOutlet } from '@angular/router';
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {TUI_DARK_MODE, TuiAppearance, TuiButton, TuiTitle, TuiIcon} from '@taiga-ui/core';
-import {TuiHeader} from '@taiga-ui/layout';
+import {TuiHeader, TuiCardLarge} from '@taiga-ui/layout';
 import { ProjectsComponent } from "./projects/projects.component";
 import { AboutComponent } from "./about/about.component";
 import { ExperienceComponent } from "./experience/experience.component";
@@ -10,7 +10,7 @@ import { P5SketchComponent } from "./p5-sketch/p5-sketch.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot, TuiButton, TuiHeader, TuiAppearance, TuiTitle, TuiIcon, ProjectsComponent, AboutComponent, ExperienceComponent, P5SketchComponent],
+  imports: [RouterOutlet, TuiRoot, TuiButton, TuiHeader, TuiAppearance, TuiTitle, TuiIcon, ProjectsComponent, AboutComponent, ExperienceComponent, P5SketchComponent, TuiCardLarge],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { TuiButton, TuiAppearance, TuiTitle, TuiSurface, TuiIcon } from '@taiga-ui/core';
+import { TuiHeader, TuiCardLarge, TuiCell } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-experience',
-  imports: [],
+  imports: [TuiCell, TuiButton, TuiHeader, TuiCardLarge, TuiAppearance, TuiTitle, TuiSurface, TuiIcon, CommonModule],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.less'
 })
